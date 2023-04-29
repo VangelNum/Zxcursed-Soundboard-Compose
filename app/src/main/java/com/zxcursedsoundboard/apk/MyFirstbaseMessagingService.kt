a18aa2924469900d5708963ff781015d2fa12ca7
VangelNum <vangelnum@gmail.com>
2023-04-30 01:24:47 +0400

    added notification with firebase

---
+++ app/src/main/java/com/zxcursedsoundboard/apk/MyFirstbaseMessagingService.kt
@@ -0,0 +1,21 @@
+package com.zxcursedsoundboard.apk
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirstbaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        if (message.notification != null) {
+            Log.d("tag", "${message.notification!!.title}")
+        } else {
+            Log.d("tag", "its null")
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+}
